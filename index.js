0cb688779fb34961065c52d14ce9e911d82a2507
jbustam9 <jbustam9@eafit.edu.co>
2021-11-18 15:54:09 -0500

    index para express

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+app.post('/', function (req, res) {
+    res.send('llegue por post a home');
+  });
+
+app.post('/about', function (req, res) {
+  res.send('post hacia about');
+});
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`);
+})
